b68a82f6bf0d6142dddbeffc1e5355e4f9735128
VladislavYuditsky <vladyusha_a@mail.ru>
2020-10-02 23:07:58 +0300

    Add students table

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,14 +1,56 @@
 import React from 'react';
+import {history} from "../utils";
+import * as axios from "axios";
+import {Table} from "react-bootstrap";
 
 class Students extends React.Component {
     constructor(props) {
         super(props);
+
+        let userData = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
+        this.state = {
+            user: userData,
+            username: userData ? userData.username : '',
+            roles: userData ? userData.roles : ['STUDENT'],
+            students: null
+        }
+    }
+
+    componentWillMount() {
+        if (!this.state.user) {
+            history.replace('/login');
+        }
+
+        axios.get('http://localhost:8080/students')
+            .then((response) => {
+                this.setState({
+                    students: response.data
+                })
+            });
     }
 
     render() {
+        const {students} = this.state;
         return (
             <div>
-                Students
+                <div className="members">
+                    {students &&
+                    <div>
+                        <h5>Students</h5>
+                        <Table>
+                            <tbody>
+                            {students.map(student => {
+                                    if (student.roles.indexOf('STUDENT') !== -1)
+                                        return <tr key={student.id}>
+                                            <td>{student.username}</td>
+                                        </tr>
+                                }
+                            )}
+                            </tbody>
+                        </Table>
+                    </div>
+                    }
+                </div>
             </div>
         )
     }
