81c32a71c1088b96609f099464b30bd9048dd323
VladislavYuditsky <vladyusha_a@mail.ru>
2020-10-07 17:44:54 +0300

    Add the ability to interact with the report

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {history} from "../utils";
 import * as axios from "axios";
-import {Button, Form} from "react-bootstrap";
+import {Button, ButtonGroup, Form, ToggleButton} from "react-bootstrap";
 
 class Settings extends React.Component {
     constructor(props) {
@@ -9,16 +9,27 @@ class Settings extends React.Component {
 
         let userData = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
         this.state = {
-            user: userData,
             username: userData ? userData.username : '',
-            email: userData ? userData.email : ''
+            enableReport: false,
+            email: userData ? userData.email : '',
+            newEmail: userData ? userData.email : '',
+            generationFrequency: 1,
+            reportId: null
         }
     }
 
     componentWillMount() {
-        if (!this.state.user) {
+        if (!this.state.username) {
             history.replace('/login');
         }
+
+        axios.get(`http://localhost:8080/report/${this.state.username}`)
+            .then((response) => {
+                this.setState({
+                    reportId: response.data.id,
+                    generationFrequency: response.data.generationFrequency,
+                })
+            })
     }
 
     handleChange = field => e => {
@@ -30,34 +41,105 @@ class Settings extends React.Component {
 
         axios.post('http://localhost:8080/user/update', {
             username: this.state.username,
-            email: this.state.email
+            email: this.state.newEmail
         })
             .then((response) => {
                 localStorage.setItem('user', JSON.stringify(response.data));
                 this.setState({
-                    user: response.date
+                    email: this.state.newEmail
                 })
             })
     }
 
+    setGenerationFrequency(value) {
+        axios.post('http://localhost:8080/report/update', {
+            id: this.state.reportId,
+            generationFrequency: value
+        })
+            .then(() => {
+                this.setState({
+                    generationFrequency: value,
+                })
+            })
+    }
+
+    changeReportState = e => {
+        e.preventDefault();
+
+        if (this.state.reportId) {
+            axios.post('http://localhost:8080/report/remove', {
+                id: this.state.reportId
+            })
+                .then(() => {
+                    this.setState({
+                        generationFrequency: 1,
+                        reportId: null
+                    })
+                })
+        } else {
+            axios.post('http://localhost:8080/report', {
+                generationFrequency: this.state.generationFrequency,
+                recipientUsername: this.state.username
+            })
+                .then((response) => {
+                    this.setState({
+                        reportId: response.data.id
+                    })
+                })
+        }
+    }
+
     render() {
-        const {email} = this.state;
+        const {newEmail, generationFrequency, reportId} = this.state;
+
+        const radios = [
+            {name: 'Day', value: '1'},
+            {name: 'Week', value: '7'},
+        ];
+
         return (
             <div>
                 <div className="members">
                     <Form onSubmit={this.handleSubmit}>
                         <Form.Group>
+                            Email
                             <Form.Control
                                 type="email"
                                 placeholder="email"
-                                value={email}
-                                onChange={this.handleChange('email')}
+                                value={newEmail}
+                                onChange={this.handleChange('newEmail')}
                             />
                         </Form.Group>
                         <Button variant="primary" type="submit" block className="form-btn">
                             Change
                         </Button>
                     </Form>
+                    <div>
+                        Report
+                        <Button variant="primary" onClick={this.changeReportState}>
+                            {reportId ? 'Disable' : 'Enable'}
+                        </Button>
+                        {reportId &&
+                        <div>
+                            Generation frequency
+                            <ButtonGroup toggle>
+                                {radios.map((radio, idx) => (
+                                    <ToggleButton
+                                        key={idx}
+                                        type="radio"
+                                        variant="secondary"
+                                        name="radio"
+                                        value={radio.value}
+                                        checked={generationFrequency === radio.value}
+                                        onChange={(e) => this.setGenerationFrequency(e.currentTarget.value)}
+                                    >
+                                        {radio.name}
+                                    </ToggleButton>
+                                ))}
+                            </ButtonGroup>
+                        </div>
+                        }
+                    </div>
                 </div>
             </div>
         )
