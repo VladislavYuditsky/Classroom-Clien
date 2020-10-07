f81607a5e6cb6f49c7ae905dba95a5fa35dedd73
VladislavYuditsky <vladyusha_a@mail.ru>
2020-10-08 00:26:22 +0300

    Add NotFound page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Redirect, Route, Router} from "react-router";
+import {Redirect, Route, Router, Switch} from "react-router";
 
 import {history} from "../utils";
 import {Login} from "./Login";
@@ -7,24 +7,28 @@ import {Members} from "./Members";
 import {Students} from "./Students"
 import {Student} from "./Student";
 import {Settings} from "./Settings";
+import {NotFound} from "./NotFound";
 
 export default () => (
     <Router history={history}>
-        <Route exact path='/'>
-            <Redirect to='/members'/>
-        </Route>
-        <Route exact path='/login'>
-            <Login/>
-        </Route>
-        <Route exact path='/members'>
-            <Members/>
-        </Route>
-        <Route exact path='/students'>
-            <Students/>
-        </Route>
-        <Route exact path='/settings'>
-            <Settings/>
-        </Route>
-        <Route path={'/student/:username'} component={Student}/>
+        <Switch>
+            <Route exact path='/'>
+                <Redirect to='/members'/>
+            </Route>
+            <Route exact path='/login'>
+                <Login/>
+            </Route>
+            <Route exact path='/members'>
+                <Members/>
+            </Route>
+            <Route exact path='/students'>
+                <Students/>
+            </Route>
+            <Route exact path='/settings'>
+                <Settings/>
+            </Route>
+            <Route path={'/student/:username'} component={Student}/>
+            <Route component={NotFound}/>
+        </Switch>
     </Router>
 );
---
+++ src/components/NotFound.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationBar} from "./Navbar";
+import {isAuthorized} from "../utils";
+
+class NotFound extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                {isAuthorized() &&
+                <NavigationBar/>
+                }
+                <div className="members">
+                    <h2>Not found 404</h2>
+                </div>
+            </div>
+        )
+    }
+}
+
+export {NotFound}
