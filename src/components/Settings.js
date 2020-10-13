e28faa4058fca7922786d0f6862d2b955fbce800
VladislavYuditsky <vladyusha_a@mail.ru>
2020-10-13 14:53:09 +0300

    Change styles

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {history, isStudent, isTeacher} from "../utils";
 import * as axios from "axios";
-import {Alert, Button, ButtonGroup, Form, ToggleButton} from "react-bootstrap";
+import {Alert, Button, ButtonGroup, Col, Form, Row, ToggleButton} from "react-bootstrap";
 import {NavigationBar} from "./Navbar";
 
 class Settings extends React.Component {
@@ -17,7 +17,8 @@ class Settings extends React.Component {
             generationFrequency: 1,
             reportId: null,
             reportError: '',
-            emailError: ''
+            emailError: '',
+            emailUpdated: false,
         }
     }
 
@@ -39,7 +40,8 @@ class Settings extends React.Component {
         this.setState({
             [field]: e.target.value,
             emailError: '',
-            reportError: ''
+            reportError: '',
+            emailUpdated: false
         });
     };
 
@@ -53,7 +55,8 @@ class Settings extends React.Component {
             .then((response) => {
                 localStorage.setItem('user', JSON.stringify(response.data));
                 this.setState({
-                    email: this.state.newEmail
+                    email: this.state.newEmail,
+                    emailUpdated: true
                 })
             })
             .catch((err) => {
@@ -110,7 +113,7 @@ class Settings extends React.Component {
     }
 
     render() {
-        const {newEmail, generationFrequency, reportId, reportError, emailError} = this.state;
+        const {newEmail, generationFrequency, reportId, reportError, emailError, emailUpdated} = this.state;
 
         const radios = [
             {name: 'Day', value: '1'},
@@ -124,46 +127,60 @@ class Settings extends React.Component {
                     {isTeacher() &&
                     <div>
                         <Form onSubmit={this.handleSubmit}>
-                            <Form.Group>
-                                Email
-                                <Form.Control
-                                    type="email"
-                                    placeholder="email"
-                                    value={newEmail}
-                                    onChange={this.handleChange('newEmail')}
-                                />
-                            </Form.Group>
-                            <Button variant="primary" type="submit" block className="form-btn">
-                                Change
-                            </Button>
+                            <Row>
+                                <Col className="col">
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="email"
+                                        value={newEmail}
+                                        onChange={this.handleChange('newEmail')}
+                                    />
+                                </Col>
+                                <Col className="col-4">
+                                    <Button variant="primary" type="submit" block className="form-btn">
+                                        Change
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Row className="mt-2 ml-1">
+                                {emailUpdated && <Alert variant="success"> Email updated successfully!</Alert>}
+                                {emailError && <Alert variant="danger">{emailError}</Alert>}
+                            </Row>
                         </Form>
-                        <div>
-                            Report
-                            <Button variant="primary" onClick={this.changeReportState}>
-                                {reportId ? 'Disable' : 'Enable'}
-                            </Button>
-                            {emailError && <Alert variant="danger">{emailError}</Alert>}
+                        <div className="mt-2">
+                            <Row>
+                                <div className="ml-3">
+                                    Report
+                                </div>
+                                <Button className="ml-4" variant="primary" onClick={this.changeReportState}>
+                                    {reportId ? 'Disable' : 'Enable'}
+                                </Button>
+                            </Row>
                             {reportId &&
-                            <div>
-                                Generation frequency
-                                <ButtonGroup toggle>
-                                    {radios.map((radio, idx) => (
-                                        <ToggleButton
-                                            key={idx}
-                                            type="radio"
-                                            variant="secondary"
-                                            name="radio"
-                                            value={radio.value}
-                                            checked={generationFrequency === radio.value}
-                                            onChange={(e) => this.setGenerationFrequency(e.currentTarget.value)}
-                                        >
-                                            {radio.name}
-                                        </ToggleButton>
-                                    ))}
-                                </ButtonGroup>
-                            </div>
+                            <Row className="mt-2 ml-1">
+                                <div>
+                                    Generation frequency
+                                    <ButtonGroup toggle className="ml-4">
+                                        {radios.map((radio, idx) => (
+                                            <ToggleButton
+                                                key={idx}
+                                                type="radio"
+                                                variant="secondary"
+                                                name="radio"
+                                                value={radio.value}
+                                                checked={generationFrequency === radio.value}
+                                                onChange={(e) => this.setGenerationFrequency(e.currentTarget.value)}
+                                            >
+                                                {radio.name}
+                                            </ToggleButton>
+                                        ))}
+                                    </ButtonGroup>
+                                </div>
+                            </Row>
                             }
-                            {reportError && <Alert variant="danger">{reportError}</Alert>}
+                            <Row className="mt-2 ml-1">
+                                {reportError && <Alert variant="danger">{reportError}</Alert>}
+                            </Row>
                         </div>
                     </div>
                     }
